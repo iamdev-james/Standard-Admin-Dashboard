0dcc31f182a31c8cd659ddd84dc4f99374ec75a4
iamdev-james <jamesoluwabukola87@gmail.com>
2022-07-21 10:28:34 -0100

    Hot Fixes

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -1,11 +1,11 @@
 import React from 'react'
 // import { BsCurrencyDollar } from 'react-icons/bs'
-// import { GoPrimitiveDot } from 'react-icons/go'
+import { GoPrimitiveDot } from 'react-icons/go'
 
-import { Button } from '../components'
-// Stacked, SparkLine, Pie,
-import { earningData} from '../data/dummy'
-// , SparklineAreaData, ecomPieChartData 
+import { Button, SparkLine } from '../components'
+// Stacked, Pie,
+import { earningData , SparklineAreaData } from '../data/dummy'
+//, ecomPieChartData 
 
 // import { useStateContext } from '../contexts/ContextProvider'
 
@@ -56,8 +56,61 @@ const Ecommerce = () => {
                   { item.percentage }
                 </span>
               </p>
+              <p className='text-gray-400 text-sm mt-1'>{ item.title }</p>
             </div>
           )) }
+        </div>
+      </div>
+      <div className='flex gap-8 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-400 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p className='text-xl font-semibold'> Revenue Updates</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 hover:drop-shadow-xl text-gray-600'>
+                <span><GoPrimitiveDot /></span>
+                <span>Expense</span>
+              </p>
+              <p className='flex items-center gap-2 hover:drop-shadow-xl text-green-400'>
+                <span><GoPrimitiveDot /></span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 border-color m-4 pr-10'>
+              {/* Budget */}
+                <div>
+                  <p>
+                    <span className='text-3xl font-semibold'>$ 23,909.67</span>
+                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
+                  </p>
+                  <p className='text-gray-500 mt-1'>
+                    Budget
+                  </p>
+                </div>
+                {/* Expense */}
+                <div className='mt-8'>
+                  <p>
+                    <span className='text-3xl font-semibold'>$ 23,909.67</span>
+                  </p>
+                  <p className='text-gray-500 mt-1'>
+                    Budget
+                  </p>
+                </div>
+                <div className='mt-5'>
+                  {/* SparK Line Area */}
+                  <SparkLine
+                    currentColor='blue'
+                    id="line-sparkline"
+                    type="Line"
+                    height="80px"
+                    width="250px"
+                    color="blue"
+                    data= {SparklineAreaData}
+                  />
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
